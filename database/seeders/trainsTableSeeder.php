d397746327f293e407cc1b7864bff84d76820b23
MonicaAuditore <mauditore@live.it>
2023-03-13 22:07:17 +0100

    add_faker

--- database/seeders/trainsTableSeeder.php
+++ database/seeders/trainsTableSeeder.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Seeder;
 //Models
 use App\Models\train;
 
+//Helpers
+use Faker\Generator as Faker;
+
 class trainsTableSeeder extends Seeder
 {
     /**
@@ -15,12 +18,13 @@ class trainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for($i = 0; $i < 2; $i++) {
         $train = new train();
         $train->Azienda = 'Trenitalia';
-        $train->Stazione_di_partenza = 'Sassari';
-        $train->Stazione_di_arrivo = 'Parigi';
+        $train->Stazione_di_partenza = 'Cuneo';
+        $train->Stazione_di_arrivo = 'Firenze';
         $train->Orario_di_partenza = '09:51:00';
         $train->Orario_di_arrivo = '20:51:00';
         $train->Codice_treno = 4536485;
@@ -28,8 +32,8 @@ class trainsTableSeeder extends Seeder
         $train->In_orario = 0;
         $train->Cancellato = 1;
         $train->Data = '2023-03-13';
-        $train->classe = 2;
+        $train->classe = $faker->buildingNumber();
         $train->save();
-
+        }
     }
 }
