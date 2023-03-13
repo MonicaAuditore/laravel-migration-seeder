306f547efba7c96f894ecb822fa2a2f1064728d2
MonicaAuditore <mauditore@live.it>
2023-03-13 19:37:43 +0100

    add_aggiornamento tabella trains

---
+++ database/migrations/2023_03_13_183053_update_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->integer('Codice Treno');
+            $table->integer('Numero Carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            //
+        });
+    }
+};
